9718156b136a95b7f4870b00ad37281ad627a180
pradyumndaga <pradyumndaga28@gmail.com>
2022-08-20 01:24:31 +0530

    swipe function and enhancement

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,33 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { MatDialogModule, MAT_DIALOG_DEFAULT_OPTIONS } from '@angular/material/dialog';
-import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
+import { Injectable, NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import {
+  MatDialogModule,
+} from "@angular/material/dialog";
+import { AppComponent } from "./app.component";
+import { HomeComponent } from "./home/home.component";
+import * as Hammer from "hammerjs";
+import {
+  HammerModule,
+  HammerGestureConfig,
+  HAMMER_GESTURE_CONFIG,
+} from "@angular/platform-browser";
+
+@Injectable()
+export class MyHammerConfig extends HammerGestureConfig {
+  override overrides = <any>{
+    swipe: { direction: Hammer.DIRECTION_ALL },
+  };
+}
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HomeComponent,
+  declarations: [AppComponent, HomeComponent],
+  imports: [BrowserModule, MatDialogModule, HammerModule],
+  providers: [
+    {
+      provide: HAMMER_GESTURE_CONFIG,
+      useClass: MyHammerConfig,
+    },
   ],
-  imports: [
-    BrowserModule,
-    MatDialogModule,
-  ],
-  providers: [],
   bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,17 +1,23 @@
-import { Component, ElementRef, HostListener, OnInit, ViewChild } from '@angular/core';
-import { Board, BoardHelper } from '../board/board';
-import { KEY_CODE } from '../app.constants';
-import { MatDialog } from '@angular/material/dialog';
-import Swal from 'sweetalert2';
-import { FormControl } from '@angular/forms';
+import {
+  Component,
+  ElementRef,
+  HostListener,
+  OnInit,
+  ViewChild,
+} from "@angular/core";
+import { Board, BoardHelper } from "../board/board";
+import { KEY_CODE } from "../app.constants";
+import { MatDialog } from "@angular/material/dialog";
+import Swal from "sweetalert2";
+import { FormControl } from "@angular/forms";
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  selector: "app-home",
+  templateUrl: "./home.component.html",
+  styleUrls: ["./home.component.css"],
 })
 export class HomeComponent implements OnInit {
-  @HostListener('window:keyup', ['$event'])
+  @HostListener("window:keyup", ["$event"])
   keyEvent(event: KeyboardEvent) {
     if (event.keyCode == KEY_CODE.DOWN_ARROW && this.Board) {
       // Your row selection code
@@ -35,40 +41,70 @@ export class HomeComponent implements OnInit {
     }
   }
 
-  img = "/assets/mario.png";
-  black = '#B24503';
-  green = '#00FF00';
-  active = 'active';
+  img = "./assets/mario.png";
+  black = "#B24503";
+  green = "#00FF00";
+  active = "active";
   length!: number;
+  gameOver = false;
   isInputError = false;
 
   Board!: Board;
-  constructor(private dialog: MatDialog) { }
+  constructor(private dialog: MatDialog) {}
 
-  ngOnInit(): void {
-
-  }
+  ngOnInit(): void {}
 
   onSubmit(event: any) {
-    if (!(event === "0" || event === "1")) {
-      debugger;
+    if (!(event === "0" || event === "1" || +event > 10)) {
       this.length = event;
       this.play();
       this.checkGreenSquareEmpty();
+      this.isInputError = false;
     } else {
-      this.isInputError = true
+      this.isInputError = true;
     }
+  }
+
+  onSwipe(event: any) {
+    debugger;
+    const x =
+      Math.abs(event.deltaX) > 40 ? (event.deltaX > 0 ? "Right" : "Left") : "";
+    const y =
+      Math.abs(event.deltaY) > 40 ? (event.deltaY > 0 ? "Down" : "Up") : "";
+    let isVertical = false;
+    isVertical = Math.abs(event.deltaY) >= Math.abs(event.deltaX);
+
+    if (x === "Right" && !isVertical) {
+      this.moveRight();
+    }
+    if (x === "Left" && !isVertical) {
+      this.moveLeft();
+    }
+    if (y === "Down" && isVertical) {
+      this.moveDown();
+    }
+    if (y === "Up" && isVertical) {
+      this.moveTop();
+    }
+  }
+
+  playAgain(): void {
+    this.length = 0;
+    this.gameOver = false;
   }
 
   checkGreenSquareEmpty() {
     if (BoardHelper.checkGreenSquares(this.Board)) {
+      this.gameOver = true;
       const msg = "Game Over! Steps taken by you:" + this.Board.player.steps;
       Swal.fire(msg);
     }
   }
 
   getEmptyBoard(): number[][] {
-    return Array.from({ length: this.length }, () => Array(this.length).fill(0));
+    return Array.from({ length: this.length }, () =>
+      Array(this.length).fill(0)
+    );
   }
 
   play() {
@@ -95,5 +131,4 @@ export class HomeComponent implements OnInit {
     this.Board = BoardHelper.movePlayerDown(this.Board, this.length);
     this.checkGreenSquareEmpty();
   }
-
 }
